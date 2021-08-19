5cc9acfda084e062a7eda92ccc4849c4257cd274
NadiiaMironenko <86535775+NadiiaMironenko@users.noreply.github.com>
2021-08-19 22:47:43 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, {useState} from "react"
-import Form from "./components/Form"
-import ToDoList from "./components/ToDoList"
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import ToDoList from "./components/ToDoList";
+import "./App.css";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setfilteredTodos] = useState([]);
+
+  useEffect(() => {
+filterHandler()
+  },[todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setfilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncomplited":
+        setfilteredTodos(todos.filter((todo) => todo.complited === false));
+        break;
+      default:
+        setfilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
@@ -16,8 +36,13 @@ function App() {
         setTodos={setTodos}
         inputText={inputText}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
-      <ToDoList setTodos={setTodos} todos={todos} />
+      <ToDoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Todo from "./Todo"
 
-const ToDo = ({ todos,setTodos }) => {
+const ToDo = ({ todos,setTodos, filteredTodos }) => {
   
     return (
       <div className="todo-container">
         <ul className="todo-list">
-          {todos.map((todo) => (
+          {filteredTodos.map((todo) => (
             <Todo
               setTodos={setTodos}
               todos={todos}
